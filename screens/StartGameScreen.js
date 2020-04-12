7bf8da36cd524b70d8e5f4bfeb8005dd467d19c1
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-12 13:31:42 +0200

    StartGameScreen component coded to contain input and buttons and styled to look nice. Color theming implementet with constant colors defined in constants/colors.js file. Card look implemented with components/Card.js component.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+import Card from '../components/Card';
+import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen} >
-            <Text>The Game Screen</Text>
+            <Text>Start a New game!</Text>
+            <Card style={styles.inputContainer}>
+                <Text style={styles.title}>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+                    </View>
+                </View>
+            </Card>
         </View>
     );
 };
@@ -14,6 +29,24 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    button: {
+        width: 100
     }
 });
 
